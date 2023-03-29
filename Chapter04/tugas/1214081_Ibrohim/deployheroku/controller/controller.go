04a296836fa8baee73859b20989bfecf4b737ee1
Ibrohim Mubarok <imubarok32@gmail.com>
2023-03-30 02:14:58 +0700

    mongodb masih belom nyambung

---
+++ Chapter04/tugas/1214081_Ibrohim/deployheroku/controller/controller.go
@@ -0,0 +1,54 @@
+package controller
+
+import (
+	"iteung/config"
+
+	"github.com/aiteung/musik"
+	"github.com/aiteung/presensi"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/websocket/v2"
+	"github.com/whatsauth/whatsauth"
+)
+
+type HTTPRequest struct {
+	Header string `json:"header"`
+	Body   string `json:"body"`
+}
+
+func Sink(c *fiber.Ctx) error {
+	var req HTTPRequest
+	req.Header = string(c.Request().Header.Header())
+	req.Body = string(c.Request().Body())
+	return c.JSON(req)
+}
+
+func WsWhatsAuthQR(c *websocket.Conn) {
+	whatsauth.RunSocket(c, config.PublicKey, config.Usertables[:], config.Ulbimariaconn)
+}
+
+func PostWhatsAuthRequest(c *fiber.Ctx) error {
+	if string(c.Request().Host()) == config.Internalhost {
+		var req whatsauth.WhatsauthRequest
+		err := c.BodyParser(&req)
+		if err != nil {
+			return err
+		}
+		ntfbtn := whatsauth.RunModuleLegacy(req, config.PrivateKey, config.Usertables[:], config.Ulbimariaconn)
+		return c.JSON(ntfbtn)
+	} else {
+		var ws whatsauth.WhatsauthStatus
+		ws.Status = string(c.Request().Host())
+		return c.JSON(ws)
+	}
+
+}
+func Homepage(c *fiber.Ctx) error {
+	ipaddr := musik.GetIPaddress()
+	return c.JSON(ipaddr)
+
+}
+
+func GetPresensiBulanIni(c *fiber.Ctx) error {
+	ps := presensi.GetPresensiCurrentMonth(config.Ulbimongoconn)
+	return c.JSON(ps)
+}
